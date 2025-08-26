9808b7d3914b2e9e30c707cb7b7295244d6156fd
Guilherme Soares <guihhsoaress@gmail.com>
2025-08-25 22:01:55 -0300

    NEW: colocada variavel de estado valor em Square e evento onClick responde com X

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 // criação do elemento squaser que irá compor o tabuleiro
 
 import './App.css';
+import {useState} from 'react';
 
-export default function Square(){
+function Square(){
+  const [valor, setValor] = useState(null);
+
+  function handleClick(){
+    setValor('X')
+  }
   return (
-    <button className="square"></button>
+    <button classeName="square" onClick={handleClick}>{valor}</button>
+  );
+}
+
+export default function Tabuleiro(){
+  return (
+    <div>
+    <div>
+    <Square valor = "1"/>
+    <Square valor = "2"/>
+    <Square valor = "3"/>
+    </div>
+    <div>
+    <Square valor = "4"/>
+    <Square valor = "5"/>
+    <Square valor = "6"/>
+    </div>
+    <div>
+    <Square valor = "7"/>
+    <Square valor = "8"/>
+    <Square valor = "9"/>
+    </div>
+    </div>
   )
 }
